085ac28aba08fff8090104d248c375d6d26d76a6
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-10-04 16:57:24 -0400

    feat: adding `stacks` module + test

---
+++ src/stacks.ts
@@ -0,0 +1,44 @@
+import test from "ava";
+
+class Stack<T> {
+  private items: Array<T>;
+
+  constructor() {
+    this.items = [];
+  }
+
+  peek = (): T => this.items[this.items.length - 1];
+
+  pop = (): T | undefined => this.items.pop();
+
+  push = (el: T): number => this.items.push(el);
+}
+
+/*
+ * @dev Reverses a string using a stack
+ * @time O(n)
+ * @space O(n)
+ */
+const reverse = (str: string): string => {
+  const stack: Stack<string> = new Stack();
+  let result = "";
+
+  // add characters to stack
+  for (let i = 0; i < str.length; i++) {
+    stack.push(str[i]);
+  }
+
+  // remove them from stack in reverse order onto the result
+  while (stack.peek()) {
+    result += stack.pop();
+  }
+
+  return result;
+};
+
+test("reverse", (t) => {
+  const actual = reverse("hello world!");
+  const expected = "!dlrow olleh";
+
+  t.is(actual, expected);
+});
