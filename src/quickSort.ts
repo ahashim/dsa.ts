1aa5eef81b93513d4a00412d4ffb5dee1fba597f
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-10-14 15:42:57 -0400

    refactor: updating `quickSort` implementation to reflect the one used in the CLRS book

--- src/quickSort.ts
+++ src/quickSort.ts
@@ -16,55 +16,50 @@ class QuickSortable {
     return this.quickSort(this.items, 0, this.items.length - 1);
   };
 
-  private partition = (
-    items: number[],
-    left: number,
-    right: number
-  ): number => {
-    // middle item
-    const pivot = items[Math.floor((right + left) / 2)];
+  private partition = (items: number[], low: number, high: number): number => {
+    const pivot = items[high];
 
-    while (left <= right) {
-      // keep moving the left pointer up if its value is below pivot
-      while (items[left] < pivot) left++;
+    // index of the smaller item (compared to the pivot value), and the current
+    // correct place for the pivot to be placed within the array
+    let i = low - 1;
 
-      // keep moving the right pointer down if its value is below pivot
-      while (items[right] > pivot) right--;
+    // iterate from `low` up to (but not including) `high`
+    for (let j = low; j <= high - 1; j++) {
+      // if the current item is smaller than the pivot    }
+      if (items[j] < pivot) {
+        // increment the index of the smaller item
+        i++;
 
-      // swap when the pointers meet
-      if (left <= right) {
-        this.swap(this.items, left, right);
-
-        // move pointers in by one
-        left++;
+        // swap it with the current item `j`
+        this.swap(items, i, j);
       }
     }
 
-    return left;
+    // swap the pivot with what's in front of its last smaller item
+    this.swap(items, i + 1, high);
+
+    // return the new pivot index
+    return i + 1;
   };
 
-  // swap two elements in an array
-  private swap = (items: number[], a: number, b: number): number[] =>
-    ([items[a], items[b]] = [items[b], items[a]]);
+  private swap = (items: number[], i: number, j: number): void => {
+    const temp = items[i];
+    items[i] = items[j];
+    items[j] = temp;
+  };
 
   private quickSort = (
     items: number[],
-    left: number,
-    right: number
+    low: number,
+    high: number
   ): number[] => {
-    // base case is when `items` is a single value
-    if (items.length > 1) {
-      // continue partitioning
-      const index = this.partition(items, left, right);
+    // base case: when low = high, there's only 1 item to return
+    if (low < high) {
+      const partitionIndex = this.partition(items, low, high);
 
-      if (left < index - 1) {
-        // left side index needs partitioning
-        this.quickSort(items, left, index - 1);
-      }
-      if (index < right) {
-        // right side of the index needs partitioning
-        this.quickSort(items, index, right);
-      }
+      // sort elements before and after the partition
+      this.quickSort(items, low, partitionIndex - 1);
+      this.quickSort(items, partitionIndex, high);
     }
 
     return items;
