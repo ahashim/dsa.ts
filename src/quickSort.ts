afa248d521259305ae3c3ff773346a38c025b0a9
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-10-14 15:52:57 -0400

    docs: updating comments in `quickSort`

--- src/quickSort.ts
+++ src/quickSort.ts
@@ -12,20 +12,24 @@ class QuickSortable {
     this.items = values;
   }
 
-  public sort = () => {
-    return this.quickSort(this.items, 0, this.items.length - 1);
+  public sort = (): number[] => {
+    const startIndex = 0;
+    const endIndex = this.items.length - 1;
+
+    return this.quickSort(this.items, startIndex, endIndex);
   };
 
   private partition = (items: number[], low: number, high: number): number => {
+    // highest index is used as the pivot point
     const pivot = items[high];
 
-    // index of the smaller item (compared to the pivot value), and the current
-    // correct place for the pivot to be placed within the array
+    // index of the currently smaller item compared to the pivot value (starts
+    // outside of the first pass)
     let i = low - 1;
 
     // iterate from `low` up to (but not including) `high`
     for (let j = low; j <= high - 1; j++) {
-      // if the current item is smaller than the pivot    }
+      // if the current item is smaller than the pivot
       if (items[j] < pivot) {
         // increment the index of the smaller item
         i++;
@@ -35,10 +39,10 @@ class QuickSortable {
       }
     }
 
-    // swap the pivot with what's in front of its last smaller item
+    // swap the pivot with what's in front of its current smaller item
     this.swap(items, i + 1, high);
 
-    // return the new pivot index
+    // return the new pivot index to partition around
     return i + 1;
   };
 
@@ -53,7 +57,8 @@ class QuickSortable {
     low: number,
     high: number
   ): number[] => {
-    // base case: when low = high, there's only 1 item to return
+    // base case: when low = high, there's only 1 item to return (and nothing
+    // left to partition)
     if (low < high) {
       const partitionIndex = this.partition(items, low, high);
 
