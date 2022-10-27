0310c7a2b3292dd5d84e9f4ea37f9756a9a28c39
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-10-27 15:30:06 -0400

    feat: adding `delete` to `heap`

--- src/heap.ts
+++ src/heap.ts
@@ -12,6 +12,32 @@ class Heap<T> {
     if (initialValue) this.data.push(initialValue);
   }
 
+  public delete = (): void => {
+    // remove last node from the heap that will trickle down from the root
+    const trickleNode = this.data.pop();
+
+    if (trickleNode) {
+      // track its index (starts at zero in order to delete the root node)
+      let trickleNodeIndex = 0;
+
+      // place trickleNode at the root to perform the deletion
+      this.data[trickleNodeIndex] = trickleNode;
+
+      // "trickle down" algorithm: loop as long as the trickle node has a child
+      // greater than it
+      while (this.hasGreaterChild(trickleNodeIndex)) {
+        // save the index of its larger child
+        const largerChildIndex = this.indexOfLargerChild(trickleNodeIndex);
+
+        // swap the trickle node with its larger child
+        this.swap(this.data, trickleNodeIndex, largerChildIndex);
+
+        // update the trickle nodes new index
+        trickleNodeIndex = largerChildIndex;
+      }
+    }
+  };
+
   public insert = (value: T): void => {
     // add it to the end of the data array
     this.data.push(value);
@@ -37,6 +63,37 @@ class Heap<T> {
 
   public rootNode = (): T => this.data[0];
 
+  private indexOfLargerChild = (index: number): number => {
+    // if there is no right child
+    if (!this.data[this.rightChildIndex(index)]) {
+      // return the left child index
+      return this.leftChildIndex(index);
+    }
+
+    // if the right child value is larger than the left child
+    if (
+      this.data[this.rightChildIndex(index)] >
+      this.data[this.leftChildIndex(index)]
+    ) {
+      // return the right child index
+      return this.rightChildIndex(index);
+    } else {
+      // return the left child index
+      return this.leftChildIndex(index);
+    }
+  };
+
+  private hasGreaterChild = (index: number): boolean => {
+    const hasGreaterLeftChild =
+      this.data[this.leftChildIndex(index)] &&
+      this.data[this.leftChildIndex(index)] > this.data[index];
+    const hasGreaterRightChild =
+      this.data[this.rightChildIndex(index)] &&
+      this.data[this.rightChildIndex(index)] > this.data[index];
+
+    return hasGreaterLeftChild || hasGreaterRightChild;
+  };
+
   private leftChildIndex = (index: number): number => index * 2 + 1;
 
   private parentIndex = (index: number): number => Math.floor((index - 1) / 2);
@@ -56,8 +113,18 @@ test("insert", (t) => {
   heap.insert(420);
   heap.insert(1337);
 
-  const actual = heap.rootNode();
-  const expected = 1337;
+  t.is(heap.rootNode(), 1337);
+  t.is(heap.lastNode(), 420);
+});
 
-  t.is(actual, expected);
+test("delete", (t) => {
+  const heap = new Heap();
+
+  heap.insert(0);
+  heap.insert(420);
+  heap.insert(1337);
+  heap.delete();
+
+  t.is(heap.rootNode(), 420);
+  t.is(heap.lastNode(), 0);
 });
