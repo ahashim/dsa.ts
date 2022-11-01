6eb92328ac473af796c647bcc25c63269382d4e5
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-11-01 15:18:37 -0400

    feat: adding vertex + test in `graph`

--- src/graph.ts
+++ src/graph.ts
@@ -1,5 +1,21 @@
 import test from "ava";
 
 /*
- * @dev Implements a graph.
+ * @dev Implements a vertex in a graph.
  */
+class Vertex<T> {
+  constructor(public value: T, public adjacentVertices: Vertex<T>[] = []) {}
+
+  public addAdjacentVertex = (vertex: Vertex<T>) =>
+    this.adjacentVertices.push(vertex);
+}
+
+test("vertices", (t) => {
+  const ahmed = new Vertex("ahmed");
+  const barbie = new Vertex("barbie");
+
+  ahmed.addAdjacentVertex(barbie);
+
+  t.is(ahmed.value, "ahmed");
+  t.is(ahmed.adjacentVertices[0].value, "barbie");
+});
