5f08298ba5994fbe3e7c0d8dd4dcfa6309c51a26
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-11-03 12:15:39 -0400

    feat: adding `Vertex` + tests in `weightedGraph`

--- src/weightedGraph.ts
+++ src/weightedGraph.ts
@@ -3,3 +3,31 @@ import test from "ava";
 /*
  * @dev: Implements a vertex in a weighted graph
  */
+class Vertex<T> {
+  constructor(
+    public value: T,
+    public adjacentVertices: {
+      [key: number]: Vertex<T>[];
+    } = {}
+  ) {}
+
+  public addEdge = (vertex: Vertex<T>, weight: number) => {
+    if (weight in this.adjacentVertices) {
+      this.adjacentVertices[weight].push(vertex);
+    } else {
+      this.adjacentVertices[weight] = [vertex];
+    }
+  };
+}
+
+test("vertices", (t) => {
+  const ahmed = new Vertex("Ahmed");
+  const barbie = new Vertex("Barbie");
+  const carlos = new Vertex("Carlos");
+
+  ahmed.addEdge(barbie, 100);
+  ahmed.addEdge(carlos, 30);
+
+  t.is(ahmed.adjacentVertices[100][0], barbie);
+  t.is(ahmed.adjacentVertices[30][0], carlos);
+});
