c51299c697cc83699c4bf1f9f46d2b26917c439b
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-11-08 09:29:20 -0500

    test: adding test cases for `debounce`

--- src/algorithms/debounce.ts
+++ src/algorithms/debounce.ts
@@ -1,3 +1,5 @@
+import test from "ava";
+
 /*
  * @dev Implements a debouncer.
  */
@@ -12,3 +14,18 @@ export const debounce = <F extends (...args: Parameters<F>) => ReturnType<F>>(
     timeout = setTimeout(() => func(...args), waitFor);
   };
 };
+
+test("debouncer", (t) => {
+  let callCount = 0;
+
+  const debounced = debounce((value: string) => {
+    ++callCount;
+    return value;
+  }, 32);
+
+  const actual = [debounced("a"), debounced("b"), debounced("c")];
+  const expected = [undefined, undefined, undefined];
+
+  t.is(callCount, 0);
+  t.deepEqual(actual, expected);
+});
