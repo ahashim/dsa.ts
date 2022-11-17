4f12697713b660de8b7fa69bca224a04f9e3c1cd
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-11-17 11:29:30 -0500

    refactor(throttle): simplifying implementation

--- src/algorithms/throttle.ts
+++ src/algorithms/throttle.ts
@@ -7,34 +7,20 @@ type Callback = (...args: any[]) => any;
  * @dev Implements a throttle control for a function.
  */
 const throttle = (fn: Callback, wait = 300): ReturnType<typeof fn> => {
-  let engaged: boolean, lastRun: number, timerID: number;
+  let engaged = false;
 
-  return function (this: any, args: any) {
-    if (!engaged) {
-      // apply local context to the function to start throttling
-      fn.apply(this, args);
+  return function (...args: any) {
+    // exit early if throttle is engaged
+    if (engaged) return;
 
-      // mark it as being ran
-      lastRun = Date.now();
+    // engage throttle
+    engaged = true;
 
-      // engage throttle
-      engaged = true;
-    } else {
-      // clear the old timer
-      clearTimeout(timerID);
+    // call the funtion
+    fn.call([...args]);
 
-      // create a new timer to engage throttle with remaining wait time
-      timerID = setTimeout(() => {
-        if (Date.now() - lastRun >= wait) {
-          // call it if its past the wait window (throttle disengaged)
-          fn.apply(this, args);
-
-          // update the last run
-          lastRun = Date.now();
-        }
-        // time remaining until it can be called again
-      }, Math.max(wait - (Date.now() - lastRun), 0));
-    }
+    // disengage after wait time
+    setTimeout(() => (engaged = false), wait);
   };
 };
 
