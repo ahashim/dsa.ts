8cb94834ed665fc8d6d7fa4307bbef8d6af6fc68
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-11-21 07:59:32 -0500

    docs(zip): updating comment on `zip` & formatting

--- src/algorithms/zip.ts
+++ src/algorithms/zip.ts
@@ -1,9 +1,9 @@
 import test from "ava";
 
 /*
- * @dev Zips the values of 2 arrays together into an output array.
+ * @dev Zips the values of 2 arrays together into an output array of tuples.
  */
-const zip =(arr1: unknown[], arr2: unknown[]): unknown[][] | null =>  {
+const zip = (arr1: unknown[], arr2: unknown[]): unknown[][] | null => {
   // cannot zip if either array is empty
   if (arr1.length === 0 || arr2.length === 0) return null;
 
@@ -14,26 +14,36 @@ const zip =(arr1: unknown[], arr2: unknown[]): unknown[][] | null =>  {
   // iterate & zip
   for (let i = 0; i < lowerBound; i++) zipped.push([arr1[i], arr2[i]]);
 
-  return zipped
-}
+  return zipped;
+};
 
-test('same sized arrays', t => {
-  const arr1 = [1,3,5,7]
-  const arr2 = [2,4,6,8]
+test("same sized arrays", (t) => {
+  const arr1 = [1, 3, 5, 7];
+  const arr2 = [2, 4, 6, 8];
 
-  t.deepEqual(zip(arr1, arr2), [[1,2],[3,4],[5,6],[7,8]])
-})
+  t.deepEqual(zip(arr1, arr2), [
+    [1, 2],
+    [3, 4],
+    [5, 6],
+    [7, 8],
+  ]);
+});
 
-test('different sized arrays', t => {
-  const arr1 = [1,3,5,7,9,11,13]
-  const arr2 = [2,4,6,8]
+test("different sized arrays", (t) => {
+  const arr1 = [1, 3, 5, 7, 9, 11, 13];
+  const arr2 = [2, 4, 6, 8];
 
-  t.deepEqual(zip(arr1, arr2), [[1,2],[3,4],[5,6],[7,8]])
-})
+  t.deepEqual(zip(arr1, arr2), [
+    [1, 2],
+    [3, 4],
+    [5, 6],
+    [7, 8],
+  ]);
+});
 
-test('an empty array', t => {
-  const arr1: number[] = []
-  const arr2 = [2,4,6,8]
+test("an empty array", (t) => {
+  const arr1: number[] = [];
+  const arr2 = [2, 4, 6, 8];
 
-  t.deepEqual(zip(arr1, arr2), null)
-})
+  t.deepEqual(zip(arr1, arr2), null);
+});
